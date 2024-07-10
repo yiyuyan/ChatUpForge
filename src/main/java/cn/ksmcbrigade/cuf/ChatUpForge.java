7d907b6d1259c1ce8dd31e08a981536c1e04db3a
yiyuyan <yiyuyan>
2024-07-10 19:30:26 +0800

    Initial commit

---
+++ src/main/java/cn/ksmcbrigade/cuf/ChatUpForge.java
@@ -0,0 +1,7 @@
+package cn.ksmcbrigade.cuf;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("cuf")
+public class ChatUpForge {
+}
---
+++ src/main/java/cn/ksmcbrigade/cuf/mixin/ChatMixin.java
@@ -0,0 +1,28 @@
+package cn.ksmcbrigade.cuf.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.ChatComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.*;
+
+@Mixin(ChatComponent.class)
+public class ChatMixin {
+
+    @Unique
+    public int chatUpForge$get(){
+        Minecraft MC = Minecraft.getInstance();
+        if(MC.player==null || MC.player.isCreative() || MC.player.isSpectator()) return 0;
+        return ((MC.player.getAbsorptionAmount()>0)?((MC.player.getArmorValue()>0?10:0)+10):(MC.player.getArmorValue()>0?10:0));
+    }
+
+    @ModifyArg(method = "render",index = 1,at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(FFF)V",ordinal = 0))
+    public float getY(float y){
+        return y - chatUpForge$get();
+    }
+
+    @ModifyConstant(method = "screenToChatY",constant = @Constant(doubleValue = 40.0D))
+    public double chatY(double constant){
+        return constant + chatUpForge$get();
+    }
+}
